848659f9799034e4e5d9d8c858ddc77b26c42959
Christopher Bautista <bautistac5198@gmail.com>
2024-10-02 14:03:34 -0700

    added functional navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Nav = () => {
+    const [hover, setHover] = useState(0);
+
+    const menuItems = [
+        { name: 'Home', link: '/' , id: 'Home'},
+        { name: 'About', link: '/about' , id: 'About'},
+        { name: 'Projects', link: '/projects' , id: 'Projects'},
+        { name: 'Contact', link: '/contact' , id: 'Contact'},
+        { name: 'Resume', link: '/resume' , id: 'Resume'}
+    ];
+
+    const handleClick = (index) => {
+        setHover(index);
+    };
+
+    const sliderPosition = `${(hover / menuItems.length) * 98}%`;
+
     return(
-        <p>Test function</p>
+        <div className='navbar'>
+            <ul>
+                {menuItems.map((item, index) => (
+                    <li key={item.id} className={item.id} onClick={() => handleClick(index)}>
+                        <a href={`#${item.id}`}>{item.name}</a>
+                    </li>
+                ))}
+                <span 
+                    className='slider' 
+                    style={{ left: sliderPosition }} // Use calculated position
+                ></span>
+            </ul>
+        </div>
     );
 };
 
